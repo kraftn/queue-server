4bae1e48f9cb839f6a8af4f4d734067ac7227f2b
Mozgolom_112 <cell-26@yandex.ru>
2019-07-25 22:22:23 +0300

    Add class for working with API of Yandex Translate

---
+++ queue-server/src/main/java/ru/practice/server/utils/YandexTranslate.java
@@ -0,0 +1,33 @@
+package utils;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class YandexTranslate {
+    private static int i = 0;
+    private static String APIkey = "trnsl.1.1.20190724T111021Z.022523749cea0e17.47536e5b755e722e5a41aa56a99671639df83f6d"; // Will be canceled in August
+
+    public static String translate(String lang, String input) throws IOException {
+        String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + APIkey;
+        URL urlObj = new URL(urlStr);
+        HttpsURLConnection connection = (HttpsURLConnection)urlObj.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+        dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + lang);
+
+        InputStream response = connection.getInputStream();
+        String json = new java.util.Scanner(response).nextLine();
+        int start = json.indexOf("[");
+        int end = json.indexOf("]");
+        String translated = json.substring(start + 2, end - 1);
+        i++;
+        if (translated.equals(input) && i < 2) {
+            // if return equal of entered text - we need change direction of translation
+            return translate("en", input);
+        } else return translated;
+    }
+}
